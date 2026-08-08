9c7b93fdfa6b14beecf3113295ddad8d5b36fb12
VishalPDSingh <vishalprasadsingh4@gmail.com>
2026-08-08 22:12:09 +0530

    first commit

--- ch4_String/Class2StringMethod/Q5_IndexOf.java
+++ ch4_String/Class2StringMethod/Q5_IndexOf.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Q5_IndexOf {
 
-    static int findChar(String str, String c)
-    {
-        char key = c.charAt(0);
+    static int findChar(String str, char key) {
 
-        for(int i=0; i<str.length(); i++)
-        {
-            if(str.charAt(i)==key)
-            {
+        for (int i = 0; i < str.length(); i++) {
+
+            if (str.charAt(i) == key) {
                 return i;
             }
         }
+
         return -1;
     }
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter String: ");
         String s = sc.nextLine();
 
-        String c = sc.nextLine();
+        System.out.print("Enter Character: ");
+        char c = sc.next().charAt(0);
+
+        System.out.println("Index: " + findChar(s, c));
+
+        sc.close();
     }
 }
